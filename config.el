536c163e5db1fef515bb7395735ca97c699b6b0f
Xiaoxing Hu <dawnstar.hu@gmail.com>
2018-05-08 14:36:39 +1200

    update config

--- config.el
+++ config.el
@@ -6,4 +6,26 @@
   :init (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
   :after magit)
 
+;; org
+(after! org
+  (setq org-directory "~/io"
+        org-agenda-files (list org-directory
+                               (concat org-directory "/notes")
+                               (concat org-directory "/work")
+                               (concat org-directory "/projects"))))
+
+(after! js2-mode
+  ;; javascript indent
+  (setq-default
+   ;; js2-mode
+   js2-basic-offset 2
+   js-indent-level 2
+   ;; web-mode
+   css-indent-offset 2
+   web-mode-markup-indent-offset 2
+   web-mode-css-indent-offset 2
+   web-mode-code-indent-offset 2
+   web-mode-attr-indent-offset 2)
+  )
+
 (provide 'config)
