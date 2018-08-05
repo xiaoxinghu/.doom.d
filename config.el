c4f065af6994e89d8c864f7728c3dee0a1d8d62f
Xiaoxing Hu <dawnstar.hu@gmail.com>
2018-08-06 10:43:34 +1200

    basic org-mode setup

--- config.el
+++ config.el
@@ -3,19 +3,44 @@
 (load! "+bindings")
 
 ;; git
-
 (def-package! magit-gitflow
   :init (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
   :after magit)
 
-;; org
-(after! org
-  (setq org-directory "~/io"
-        org-agenda-files (list org-directory
-                               (concat org-directory "/notes")
-                               (concat org-directory "/work")
-                               (concat org-directory "/projects")))
 
+;; org
+(let* ((root "~/io")
+       (inbox (concat root "/inbox.org"))
+       (gtd (concat root "/gtd.org")))
+  (setq
+   org-directory root
+   org-agenda-files (list inbox gtd)))
+
+;;; default settings
+(setq
+   org-log-into-drawer 1
+   org-archive-mark-done nil)
+
+;;; capture
+(setq org-capture-templates
+      `(("t" "todo" entry
+         (file+headline ,(concat org-directory "/inbox.org") "Tasks")
+         (file , "~/.doom.d/templates/todo.txt")
+         ::empty-lines-before 1
+         ::empty-lines-after 1)
+        ("n" "note" entry
+         (file+headline ,(concat org-directory "/inbox.org") "Notes")
+         (file , "~/.doom.d/templates/note.txt")
+         ::empty-lines-before 1
+         ::empty-lines-after 1)
+        ("l" "link" entry
+         (file+headline ,(concat org-directory "/inbox.org") "Notes")
+         (file , "~/.doom.d/templates/link.txt")
+         ::empty-lines-before 1
+         ::empty-lines-after 1)
+        ))
+
+(after! org
   (map! :map evil-org-mode-map
         :localleader
         :n "l" #'x/org-insert-link-with-title)
